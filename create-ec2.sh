74fa9fb1e4bbae6d384a39ba88f2f5b28ee5e1b1
srinivas <skasetti@gmail.com>
2023-08-30 18:11:26 -0400

    added script

--- create-ec2.sh
+++ create-ec2.sh
@@ -4,6 +4,7 @@ NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping"
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-01753dd42fcf3cbf2
+DOMAIN_NAME=practiceonline.online
 # if mysql or mongodb instance_type should be t3.medium, for all others it is t2.micro
 
 for i in "${NAMES[@]}"
@@ -19,4 +20,18 @@ do
     $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].
     PrivateIpAddress')
      echo "created $i instance: $IP_ADDRESS"
+
+aws route53 change-resource-record-sets --hosted-zone-id Z0638654187J160VKOIY1 --change-batch ' 
+{
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                            "Name": "'$i.$DOMAIN_NAME'",
+                            "Type": "A",
+                            "TTL": 300,
+                            "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+                        }}]
+    }
+    '
+
 done
